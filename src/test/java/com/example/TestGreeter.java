618a6d80d2346e4347ba30de925fcdca0ae158f8
rhodinemma <rhodinemma10@gmail.com>
2023-01-21 18:13:57 +0300

    ft(trigger-jenkins)

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -34,9 +34,16 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you for blessing me";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
